922477ba1fe107de4a0fd085f605071d5b061845
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-03-04 14:03:24 +0530

    hello

---
+++ index.cpp
@@ -0,0 +1,42 @@
+
+que-----------------1--------------------------
+#include <iostream>
+using namespace std;
+int main(){
+    // int num=1;
+    // for(int i=1; i<=4; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+
+que-----------------2--------------------------
+
+
+// for(int i=0; i<=5; i++){
+//     for(int s=5; s>i; s--){
+//         cout<<"  ";
+//     }
+//     for(int h=1; h<=i; h++){
+//         cout<<" *  ";
+// }
+//    cout<<endl;
+
+// }
+
+
+que-----------------3--------------------------
+
+
+for(int i=1; i<=5;i++){
+    if(i==1 || i==3){
+        cout<<"* * * * *";
+    }else{
+        cout<<"*       *";
+    }
+    cout<<endl;
+}
+}
